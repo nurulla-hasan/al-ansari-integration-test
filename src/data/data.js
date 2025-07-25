1c3a4867c03e899eca71d5532be2efea3dd591ed
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-07-25 17:38:08 +0600

    delete unused data

--- src/data/data.js
+++ src/data/data.js
@@ -1,52 +1,4 @@
 
-// Services Data
-export const servicesItems = [
-  {
-    "_id": "service1",
-    "title": "Local Expertise, Global Standards",
-    "description": "We provide legal solutions grounded in Qatari law and shaped by international standards. Our approach ensures clarity and confidence for both local and multinational clients.",
-    "image": "/assets/service-01.jpg"
-  },
-  {
-    "_id": "service2",
-    "title": "Industry-Focused Solutions",
-    "description": "With deep insight into sectors like construction, energy, finance, and technology, we tailor our legal strategies to meet the specific challenges of each industry.",
-    "image": "/assets/service-02.png"
-  },
-  {
-    "_id": "service3",
-    "title": "Trusted Legal Representation",
-    "description": "From corporate advisory to dispute resolution and compliance, we deliver effective legal support focused on results, professionalism, and long-term client value.",
-    "image": "/assets/service-03.jpg"
-  }
-]
-
-// About Data
-export const aboutData = [
-  {
-    "_id": "667e4e8a9b2f3e4c5d6f7b01",
-    "date": "16 March, 2025",
-    "title": "Legal Practice",
-    "summary": "Our practice is underpinned by a deep understanding of local laws and of the sectors in which our clients operate. We provide expert counsel and innovative solutions to complex issues.",
-    "imageUrl": "/assets/legal-practice.png"
-  },
-  {
-    "_id": "667e4e8ased9b2f3e4c5d6f7b01",
-    "date": "16 March, 2025",
-    "title": "Client Focus",
-    "summary": "We pride ourselves on providing efficient, responsive and courteous service. We add value for our clients by providing training and information to support their business in the region.",
-    "imageUrl": "/assets/Client Focus.jpg"
-  },
-  {
-    "_id": "667e4e8dfda9b2f3e4c5d6f7b01",
-    "date": "16 March, 2025",
-    "title": "Highest Level",
-    "summary": "We are proud to offer legal services and expertise with international standards. Our core strength is the diversity and experience of our legal professionals with commitment to the highest standards.",
-    "imageUrl": "/assets/Highest Level.jpg"
-  },
-]
-
-
 // Contact Data
 export const contactData =
 {
@@ -54,4 +6,4 @@ export const contactData =
   "email": "info@alansarilaw.com",
   "phone": "Dial: +974.4491.3355 || Fax: +974.4491.3356",
   "officeHours": "Sunday to Thursday 8am to 5pm"
-}
+} 
