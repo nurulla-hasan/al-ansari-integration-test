68a08f166c55c786c7482b5f2805d5cfeeafa54b
nurulla-hasan <nurullahasan.dev@gmail.com>
2025-07-28 15:04:52 +0600

    add domain in next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,8 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: '13.62.48.83', 
-        port: '5001',         
-        pathname: '/**', 
-      }
-    ],
+    domains: ['backend.alansarilaw.com'],
   },
-};
+}
 
 export default withNextIntl(nextConfig);
